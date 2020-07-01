2899033e809abb35f7146fcb6d13e430488f2711
Abhishek Sarwan <abhisheksarwan@icloud.com>
2020-07-01 19:34:12 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popup = document.getElementById('pop');
+    popup.classList.toggle('active');
+}
